7faedff31352a6c377af5ace0ecf25309f874b8e
James O'Neal <jamesponeal@gmail.com>
2017-01-29 11:28:10 -0800

    moved isogram functions inside constructor function

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,31 +1,54 @@
+// var Isogram = function(word){
+//   this.word = word;
+// };
+
+// function prepareWord(inputWord){
+//   var firstClean = inputWord.replace(/[@.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+//   var secondClean = firstClean.replace(/\s+/g,"");
+//   return secondClean.toLowerCase().split("");
+// };
+
+// Isogram.prototype.isIsogram = function(){
+//   var arrayOfLetters = prepareWord(this.word);
+//   var letterCount = {};
+//   var isogram = true;
+//   for(var i = 0; i < arrayOfLetters.length; i++){
+//     if(letterCount.hasOwnProperty(arrayOfLetters[i])){
+//       letterCount[arrayOfLetters[i]] += 1;
+//       isogram = false;
+//     } else {
+//       letterCount[arrayOfLetters[i]] = 1;
+//     }
+//   }
+//   return isogram;
+// }
+
+
+
 var Isogram = function(word){
-  this.word = word;
-  // cleanWord : function(inputWord){
-  //   var firstClean = inputWord.replace(/[@.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-  //   var secondClean = firstClean.replace(/\s+/g,"");
-  //   return secondClean.toLowerCase().split("");
-  // };
-};
+  this.word = word,
 
-function prepareWord(inputWord){
-  var firstClean = inputWord.replace(/[@.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-  var secondClean = firstClean.replace(/\s+/g,"");
-  return secondClean.toLowerCase().split("");
-};
+  this.prepareWord = function (inputWord){
+    var firstClean = inputWord.replace(/[@.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var secondClean = firstClean.replace(/\s+/g,"");
+    return secondClean.toLowerCase().split("");
+  },
 
-Isogram.prototype.isIsogram = function(){
-  var arrayOfLetters = prepareWord(this.word);
-  var letterCount = {};
-  var isogram = true;
-  for(var i = 0; i < arrayOfLetters.length; i++){
-    if(letterCount.hasOwnProperty(arrayOfLetters[i])){
-      letterCount[arrayOfLetters[i]] += 1;
-      isogram = false;
-    } else {
-      letterCount[arrayOfLetters[i]] = 1;
+  this.isIsogram = function(){
+    var arrayOfLetters = this.prepareWord(this.word);
+    var letterCount = {};
+    var isogram = true;
+    for(var i = 0; i < arrayOfLetters.length; i++){
+      if(letterCount.hasOwnProperty(arrayOfLetters[i])){
+        letterCount[arrayOfLetters[i]] += 1;
+        isogram = false;
+      } else {
+        letterCount[arrayOfLetters[i]] = 1;
+      }
     }
+    return isogram;
   }
-  return isogram;
 }
+
 
 module.exports = Isogram;
