1425aacc63c734ffaef65c22fdde00d285ad3606
James O'Neal <jamesponeal@gmail.com>
2017-02-27 20:17:59 -0800

    adds hamming solution

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,25 +1,24 @@
+module BookKeeping
+  VERSION = 3 # Where the version number matches the one in the test.
+end
+
 class Hamming
 
-  def self.compute(dna1, dna2)
-    array1 = dna1.split("")
-    array2 = dna2.split("")
+  def self.compute(a, b)
+
     count = 0
     i = 0
-    if array1.length == array2.length
-      while i < array1.length
-        if array1[i] != array2[i]
+    if a.length == b.length
+      for i in 0..a.length
+        if a[i] != b[i]
           count += 1
         end
-        i += 1
       end
     else
       raise ArgumentError.new("Different lengths not allowed!")
     end
-    return count
+    count
   end
 
 end
 
-module BookKeeping
-  VERSION = 3 # Where the version number matches the one in the test.
-end
